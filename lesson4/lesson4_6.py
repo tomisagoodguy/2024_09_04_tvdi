eb9b96eca26cb708943d1eb84206ae1ed8a8c8af
Tom chen <tom890108159@gmail.com>
2024-10-18 16:49:52 +0800

    update

---
+++ lesson4/lesson4_6.py
@@ -0,0 +1,29 @@
+from ttkthemes import ThemedTk
+from tkinter import ttk
+
+
+class Window(ThemedTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title('使用ttk的套件')
+        style = ttk.Style(self)
+        topFrame = ttk.Frame(self, borderwidth=1, relief='groove')
+        btn1 = ttk.Button(topFrame, text="按鈕1")
+        btn1.pack(side='left')
+        btn2 = ttk.Button(topFrame, text="按鈕2")
+        btn2.pack(side='left')
+        btn3 = ttk.Button(topFrame, text="按鈕3")
+        btn3.pack(side='left')
+        topFrame.pack(padx=10, pady=(10, 0), expand=True, fill='x')
+        bottomFrame = ttk.Frame(
+            self, width=500, height=300, borderwidth=1, relief='groove')
+        bottomFrame.pack(padx=10, pady=10)
+
+
+def main():
+    window = Window(theme="arc")
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
