f9e049c5c0577fe0123007bd33686818c96bdc45
Tom Chen <tom890108159@gmail.com>
2024-11-04 19:48:28 +0800

    update

--- lesson7/datasource.py
+++ lesson7/datasource.py
@@ -2,54 +2,78 @@ import requests
 import sqlite3
 
 
-
-def get_sitename() -> list[str]:
+def get_sitename(county: str) -> list[str]:
     '''
-    doccString
+    docString
+    parameter:
+        county:城市名稱
+
+    return:
+        傳出所有的站點名稱
+    '''
+    conn = sqlite3.connect("AQI.db")
+    with conn:
+        # Create a cursor object to execute SQL commands
+        cursor = conn.cursor()
+        # SQL query to select unique sitenames from records table
+        sql = '''
+        SELECT DISTINCT sitename
+        FROM records
+        WHERE county = ?       
+         '''
+        # Execute the SQL query
+        cursor.execute(sql, (county,))
+        sitenames = [items[0] for items in cursor.fetchall()]
+
+    # Return the list of unique sitenames
+    return sitenames
+
+
+def get_county() -> list[str]:
+    '''
+    docString
     parameter:
     return:
-        傳出所有站點名稱
+        傳出所有的城市名稱
+    '''
+    conn = sqlite3.connect("AQI.db")
+    with conn:
+        # Create a cursor object to execute SQL commands
+        cursor = conn.cursor()
+        # SQL query to select unique sitenames from records table
+        sql = '''
+        SELECT DISTINCT county
+        FROM records
+        '''
+        # Execute the SQL query
+        cursor.execute(sql)
+        # Get all results and extract first item from each row into a list
+        counties = [items[0] for items in cursor.fetchall()]
 
+    # Return the list of unique sitenames
+    return counties
+
+
+def get_selected_data(sitename: str) -> list[list]:
+    '''
+    使用者選擇了sitename,並將sitename傳入
+    Parameter:
+        sitename: 站點的名稱
+    Return:
+        所有關於此站點的相關資料
     '''
     conn = sqlite3.connect("AQI.db")
     with conn:
         cursor = conn.cursor()
         sql = '''
-        SELECT DISTINCT sitename
+        SELECT date,county,aqi,pm25,status,lat,lon
         FROM records
+        WHERE sitename=?
+        ORDER BY date DESC;
         '''
-        cursor.execute(sql)
-        sitenames = [items[0] for items in cursor.fetchall()]
-
-    return sitenames
-
-
-def get_selected_data(sitename: str) -> list[list]:
-    url = 'https://data.moenv.gov.tw/api/v2/aqx_p_488?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON'
-    
-    '''
-    使用者選擇了sitename，並將sitename傳入
-    parameter:
-        sitename: 站點的名稱
-    Return:
-        所有關於此站點的相關資料
-    
-    '''
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.json()
-    except Exception as e:
-        print(e)
-    else:
-        outerlist = []
-        for items in data['records']:
-            if items['sitename'] == sitename:
-                innerlist = [items['datacreationdate'], items['county'], items['aqi'],
-                             items['pm2.5'], items['status'], items['latitude'], items['longitude']]
-                outerlist.append(innerlist)
-
-        return outerlist
+        cursor.execute(sql, (sitename,))
+        sitename_list = [list(item) for item in cursor.fetchall()]
+        return sitename_list
 
 
 def download_data():
--- lesson7/lesson7_1.py
+++ lesson7/lesson7_1.py
@@ -27,13 +27,23 @@ class Window(ThemedTk):
 
         # ==============bottomFrame===============
         bottomFrame = ttk.Frame(self)
-        sitenames = datasource.get_sitename()
-        self.selected_site = tk.StringVar()
+        # ==============SelectedFrame===============
+        self.selectedFrame = ttk.Frame(self, padding=[10, 10, 10, 10])
+        # combobox選擇城市
+        counties = datasource.get_county()
+        # self.selected_site = tk.StringVar()
+        self.selected_county = tk.StringVar()
         sitenames_cb = ttk.Combobox(
-            bottomFrame, textvariable=self.selected_site, values=sitenames, state='readonly')
-        self.selected_site.set('請選擇站點')
-        sitenames_cb.bind('<<ComboboxSelected>>', self.sitename_selected)
-        sitenames_cb.pack(side='left', expand=True, anchor='n')
+            self.selectedFrame, textvariable=self.selected_county, values=counties, state='readonly')
+        self.selected_county.set('請選擇城市')
+        sitenames_cb.bind('<<ComboboxSelected>>', self.county_selected)
+        sitenames_cb.pack(anchor='n', pady=10)
+
+        self.listbox = None
+
+        self.selectedFrame.pack(
+            side='left', expand=True, fill='y', padx=(20, 0))
+        # ==============End SelectedFrame===============
 
         # define columns
         columns = ('date', 'county', 'aqi', 'pm25', 'status', 'lat', 'lon')
@@ -72,6 +82,21 @@ class Window(ThemedTk):
 
         # ==============end bottomFrame===============
 
+    def county_selected(self, event):
+        selected = self.selected_county.get()
+        counties = datasource.get_sitename(county=selected)
+        # listbox選擇站點
+        if self.listbox:
+            self.listbox.destroy()
+        var = tk.Variable(value=counties)
+        self.listbox = tk.Listbox(
+            self.selectedFrame,
+            listvariable=var,
+            height=6,
+            selectmode=tk.EXTENDED
+        )
+        self.listbox.pack()
+
     def sitename_selected(self, event):
         for children in self.tree.get_children():
             self.tree.delete(children)
@@ -89,4 +114,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
